e3c5a2ba5b1c7c8566bf229212e01b97a723e310
Alexis Low <alexisthelow@gmail.com>
2017-08-11 11:14:35 -0600

    third?

--- ReceiptRevisitedException.java
+++ ReceiptRevisitedException.java
@@ -1,8 +1,18 @@
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class ReceiptRevisitedException {
 	public static void main(String [] args) throws Exception {
+		Scanner scanner = new Scanner(System.in);
+		double gasPrice = 2.299;
+		
+		System.out.print("How many gallons of gas do you want? > ");
+		double gasPurchase = scanner.nextDouble();
+		
+		double gasTotal = gasPrice * gasPurchase;
+		
+		
 		PrintWriter fileout;
 
 		try {
@@ -21,10 +31,10 @@ public class ReceiptRevisitedException {
 		fileout.println("|                        |");
 		fileout.println("| 2017-08-01  14:29pm    |");
 		fileout.println("|                        |");
-		fileout.println("| Gallons:       10.870  |");
+		fileout.println("| Gallons:        " + gasPurchase + "   |");
 		fileout.println("| Price/gallon: $ 2.299  |");
 		fileout.println("|                        |");
-		fileout.println("| Fuel total:   $ 24.99  |");
+		fileout.println("| Fuel total:   $ " + gasTotal + "  |");
 		fileout.println("|                        |");
 		fileout.println("+------------------------+");
 		fileout.close();
